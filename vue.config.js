155a12232bcadd6c991c7708085bbee7ddf291d2
ACS106103 <acs106103@gm.ntcu.edu.tw>
2021-07-09 22:49:50 +0800

    update

---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/ntcu-course-frontend-via-webpack/'
+        : '/'
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,20 @@
-const path = require("path");
-const common = require("./webpack.common");
-const { merge } = require('webpack-merge');
-
+const path = require('path')
+const common = require('./webpack.common')
+const { merge } = require('webpack-merge')
 
 module.exports = merge(common, {
-  mode: "production",
-  output: {
-    filename: "[name].[contentHash].bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  plugins: [
+    mode: 'production',
+    output: {
+        // filename: '[name].[contentHash].bundle.js',
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
 
-  ],
-  module: {
-    rules: [
-      
-    ]
-  }
-});
+    ],
+    module: {
+        rules: [
+
+        ]
+    }
+})
